fab7e3dba5ac1287a7bc3ec9f0259d87d36fdfc4
Amara Lovato <amaracuenca@gmail.com>
2017-01-24 14:43:35 -0700

    adds sinatra app shopping

--- shopping_app.rb
+++ shopping_app.rb
@@ -5,20 +5,40 @@ require 'sinatra/reloader'
 
 get '/' do
   @name = params["name"]
-
+  @price = params["price"]
   erb :index
+end
+
+get '/items' do
+  #here is where it's supposed to be the code
+end
+
+get '/cart' do
+  @item = params["item"]
+  @price = params["price"]
+  erb :cart
 end
 
 # Practice with Form Params
 
 get '/items/new' do
+  @item = params["item"]
+  @price = params["price"]
   erb :new_item
 end
 
 post '/items' do
+  @item = params["item"]
+  @price = params["price"]
   erb :cart
 end
 
 get '/users/new' do
   erb :new_user
+end
+
+post '/users' do
+  @username = params["username"]
+  @password = params["password"]
+  erb :user
 end
